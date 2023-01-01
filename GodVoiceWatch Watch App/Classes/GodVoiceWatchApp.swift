2cb9324a3edc3d1db51d3b9e1e714aaa2253979d
subta90 <the.bird.nobility298@gmail.com>
2023-01-01 15:19:40 +0900

    initial commit

---
+++ GodVoiceWatch Watch App/Classes/GodVoiceWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GodVoiceWatchApp.swift
+//  GodVoiceWatch Watch App
+//
+//  Created by subta on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct GodVoiceWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GodVoiceWatch/Classes/GodVoiceWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GodVoiceWatchApp.swift
+//  GodVoiceWatch
+//
+//  Created by subta on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct GodVoiceWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
